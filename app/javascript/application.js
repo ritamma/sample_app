2f453cd49866ba167ad352db9727149f9b89dc2f
ritamma <rita_meschanskaya@mail.ru>
2022-08-18 15:04:37 +0300

    Finish log in/log out

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+// import "controllers"
+// import "jquery"
+// import "jqury_ujs"
+// import "popper"
+// import "bootstrap"
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,12 +48,22 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
+   @user.save
     assert_not duplicate_user.valid?
   end
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
